7babba9eeb2101c834c4ff85baaa99f46232ff9c
dev-abuhasan <ahinstitutte@gmail.com>
2022-03-03 20:56:42 +0600

    user profile

--- pages/my-account/feed.tsx
+++ pages/my-account/feed.tsx
@@ -1,13 +1,14 @@
+import type { NextPage } from 'next';
 import UserAccountLayout from '../../components/Layouts/UserAccountLayout/UserAccountLayout';
 import { PageSeo } from '../../components/Seo/PageSeo';
 
-const Feed = () => {
+const Feed: NextPage = () => {
     return (
         <>
             <PageSeo title={`Feed — `} />
             <UserAccountLayout>
                 <div>
-                    <h1 className="text-3xl">Your Feed</h1>
+                    <h1 className="text-3xl">Feed List</h1>
                 </div>
             </UserAccountLayout>
         </>
