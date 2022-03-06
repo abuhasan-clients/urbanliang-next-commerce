df3e5e60f12a7fb6eb4598a54af308e9ba8fedc8
unknown <shakib.srs61@gmail.com>
2022-03-06 17:24:41 +0600

    95% completed seller page

---
+++ pages/pages/sellers/index.tsx
@@ -0,0 +1,197 @@
+import type { NextPage } from 'next';
+import { AiFillStar } from 'react-icons/ai';
+import SignupMail from '../../../components/Pagescompo/Home/SignupMail';
+
+const Sellers: NextPage = () => {
+    return (
+        <>
+            <h1 className="mt-8   text-5xl tracking-tight text-gray  flex items-center justify-center">OUR CREATORS</h1>
+            <p className="mt-5   text-lg  text-gray-900  flex items-center justify-center">
+                Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum
+            </p>
+            <p className=" text-lg text-gray-900  flex items-center justify-center"> giving information on its originss</p>
+
+            <h1 className="mt-8   text-7xl tracking-tight text-orange-400  flex items-center justify-center">A</h1>
+            <div className="mt-4 lg:w-5/6 xl:w-2/3 md:w-full sm:w-full flex justify-center mx-auto pt-16">
+                <div className="grid sm:grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 pb-6   ">
+                    <div className="focus:outline-none cardAll" aria-label="card 2" id=" ">
+                        <img
+                            role="img"
+                            aria-label="gaming"
+                            className="focus:outline-none focus:outline-none imgSize"
+                            src="https://us.123rf.com/450wm/melnyk58/melnyk581803/melnyk58180301046/97316365-full-length-of-young-slim-female-girl-in-denim-shorts-on-gray-background.jpg"
+                            alt="notes"
+                        />
+
+                        <div className="bg-white  py-4  ">
+                            <p className="text-gray text-xl mt-3">CLOTH</p>
+                            <h1 className="focus:outline-none flex mt-1   text-lg text-gray-900 font-semibold tracking-wider">
+                                <AiFillStar className="starReview" />
+                                <AiFillStar className="starReview" />
+                                <AiFillStar className="starReview" />
+                                <AiFillStar className="starReview" />
+                            </h1>
+                            <small>128 Reviews</small>
+
+                            <div className="ml-5">
+                                <ul className="list-disc text-gray-700">
+                                    <li>additional values specific</li>
+                                    <li>additional values specific</li>
+                                    <li>additional specific</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="focus:outline-none cardAll" aria-label="card 2" id=" ">
+                        <img
+                            role="img"
+                            aria-label="gaming"
+                            className="focus:outline-none focus:outline-none imgSize"
+                            src="https://i.pinimg.com/originals/8e/ad/e4/8eade4befcb9a42f71bd0742241be2a9.jpg"
+                            alt="notes"
+                        />
+
+                        <div className="bg-white  py-4  ">
+                            <p className="text-gray text-xl mt-3">CLOTH</p>
+                            <h1 className="focus:outline-none flex mt-1   text-lg text-gray-900 font-semibold tracking-wider">
+                                <AiFillStar className="starReview" />
+                                <AiFillStar className="starReview" />
+                                <AiFillStar className="starReview" />
+                                <AiFillStar className="starReview" />
+                            </h1>
+                            <small>128 Reviews</small>
+
+                            <div className="ml-5">
+                                <ul className="list-disc text-gray-700">
+                                    <li>additional values specific</li>
+                                    <li>additional values specific</li>
+                                    <li>additional specific</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="focus:outline-none cardAll" aria-label="card 2" id=" ">
+                        <img
+                            role="img"
+                            aria-label="gaming"
+                            className="focus:outline-none focus:outline-none imgSize"
+                            src="https://media.vogue.fr/photos/5d3985538602c4000a1fcb2b/3:4/w_831,h_1108,c_limit/180411_CM_ParisVogue_03_0506_R3.jpg"
+                            alt="notes"
+                        />
+
+                        <div className="bg-white  py-4  ">
+                            <p className="text-gray text-xl mt-3">CLOTH</p>
+                            <h1 className="focus:outline-none flex mt-1   text-lg text-gray-900 font-semibold tracking-wider">
+                                <AiFillStar className="starReview" />
+                                <AiFillStar className="starReview" />
+                                <AiFillStar className="starReview" />
+                                <AiFillStar className="starReview" />
+                            </h1>
+                            <small>128 Reviews</small>
+
+                            <div className="ml-5">
+                                <ul className="list-disc text-gray-700">
+                                    <li>additional values specific</li>
+                                    <li>additional values specific</li>
+                                    <li>additional specific</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* added  b*/}
+            <h1 className="mt-8   text-7xl tracking-tight text-orange-400  flex items-center justify-center">B</h1>
+            <div className="mt-4 lg:w-5/6 xl:w-2/3 md:w-full sm:w-full flex justify-center mx-auto pt-16">
+                <div className="grid sm:grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 pb-6   ">
+                    <div className="focus:outline-none cardAll" aria-label="card 2" id=" ">
+                        <img
+                            role="img"
+                            aria-label="gaming"
+                            className="focus:outline-none focus:outline-none imgSize"
+                            src="https://us.123rf.com/450wm/melnyk58/melnyk581803/melnyk58180301046/97316365-full-length-of-young-slim-female-girl-in-denim-shorts-on-gray-background.jpg"
+                            alt="notes"
+                        />
+
+                        <div className="bg-white  py-4  ">
+                            <p className="text-gray text-xl mt-3">CLOTH</p>
+                            <h1 className="focus:outline-none flex mt-1   text-lg text-gray-900 font-semibold tracking-wider">
+                                <AiFillStar className="starReview" />
+                                <AiFillStar className="starReview" />
+                                <AiFillStar className="starReview" />
+                                <AiFillStar className="starReview" />
+                            </h1>
+                            <small>128 Reviews</small>
+
+                            <div className="ml-5">
+                                <ul className="list-disc text-gray-700">
+                                    <li>additional values specific</li>
+                                    <li>additional values specific</li>
+                                    <li>additional specific</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="focus:outline-none cardAll" aria-label="card 2" id=" ">
+                        <img
+                            role="img"
+                            aria-label="gaming"
+                            className="focus:outline-none focus:outline-none imgSize"
+                            src="https://i.pinimg.com/originals/8e/ad/e4/8eade4befcb9a42f71bd0742241be2a9.jpg"
+                            alt="notes"
+                        />
+
+                        <div className="bg-white  py-4  ">
+                            <p className="text-gray text-xl mt-3">CLOTH</p>
+                            <h1 className="focus:outline-none flex mt-1   text-lg text-gray-900 font-semibold tracking-wider">
+                                <AiFillStar className="starReview" />
+                                <AiFillStar className="starReview" />
+                                <AiFillStar className="starReview" />
+                                <AiFillStar className="starReview" />
+                            </h1>
+                            <small>128 Reviews</small>
+
+                            <div className="ml-5">
+                                <ul className="list-disc text-gray-700">
+                                    <li>additional values specific</li>
+                                    <li>additional values specific</li>
+                                    <li>additional specific</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="focus:outline-none cardAll" aria-label="card 2" id=" ">
+                        <img
+                            role="img"
+                            aria-label="gaming"
+                            className="focus:outline-none focus:outline-none imgSize"
+                            src="https://media.vogue.fr/photos/5d3985538602c4000a1fcb2b/3:4/w_831,h_1108,c_limit/180411_CM_ParisVogue_03_0506_R3.jpg"
+                            alt="notes"
+                        />
+
+                        <div className="bg-white  py-4  ">
+                            <p className="text-gray text-xl mt-3">CLOTH</p>
+                            <h1 className="focus:outline-none flex mt-1   text-lg text-gray-900 font-semibold tracking-wider">
+                                <AiFillStar className="starReview" />
+                                <AiFillStar className="starReview" />
+                                <AiFillStar className="starReview" />
+                                <AiFillStar className="starReview" />
+                            </h1>
+                            <small>128 Reviews</small>
+
+                            <div className="ml-5">
+                                <ul className="list-disc text-gray-700">
+                                    <li>additional values specific</li>
+                                    <li>additional values specific</li>
+                                    <li>additional specific</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <SignupMail />
+        </>
+    );
+};
+export default Sellers;
