80ac25eaeb4c37068f9341bc58ad8c613d6abb1f
devabuhasan <dev.abuhasan@gmail.com>
2022-03-10 08:30:35 -0500

    remove semi bold

--- components/Reuseable/Banner.tsx
+++ components/Reuseable/Banner.tsx
@@ -7,7 +7,7 @@ const Banner = ({ image, desc, textColor, title }: any) => {
         <div className="relative overflow-hidden  " id={styles.bannerMain}>
             <img className="object-cover w-full " src={image} alt="Flower and sky" />
             <div className="absolute pt-12  px-6 py-4" id={styles.mainClass}>
-                <div className={`mb-3 sm:text-4xl  xl:text-8xl md:text-8xl  font-semibold tracking-tight ${textColor}`}>{title}</div>
+                <div className={`mb-3 sm:text-4xl  xl:text-8xl md:text-8xl tracking-tight ${textColor}`}>{title}</div>
                 <div className={`leading-normal ${textColor}`}>{desc}</div>
             </div>
         </div>
