6f8f077fd5dfdade2d7b7452a07d10daaf00d8b7
unknown <shakib.srs61@gmail.com>
2022-03-10 20:05:57 +0600

    aaded

---
+++ components/Reuseable/BannerForCenter.tsx
@@ -0,0 +1,17 @@
+import styles from '../../styles/Reuseable/Banner.module.scss';
+
+const BannerForCenter = ({ image, desc, textColor, title }: any) => {
+    // layer, defaultInfo, rightInfo, defaultClasses, icon
+
+    return (
+        <div className="relative overflow-hidden w-full" id={styles.bannerMain}>
+            <img className="object-cover w-full " src={image} alt="Flower and sky" />
+            <div className="absolute pt-12  px-6 py-4" id={styles.mainClass}>
+                <div className={`mb-3 sm:text-4xl  xl:text-8xl md:text-8xl tracking-tight ${textColor}`}>{title}</div>
+                <div className={`leading-normal ${textColor}`}>{desc}</div>
+            </div>
+        </div>
+    );
+};
+
+export default BannerForCenter;
