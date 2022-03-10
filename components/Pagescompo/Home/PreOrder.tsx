dc26a76fed3472b657697aa9b3b1a1e8bb9d55a7
unknown <shakib.srs61@gmail.com>
2022-03-10 20:40:41 +0600

    aaded

--- components/Pagescompo/Home/PreOrder.tsx
+++ components/Pagescompo/Home/PreOrder.tsx
@@ -4,7 +4,7 @@ import { AiFillStar } from 'react-icons/ai';
 const PreOrder: NextPage = () => {
     return (
         <>
-            <p className="text-5xl mb-12 ml-3 mt-12">PRE-ORDER</p>
+            <p className="text-5xl mb-12 ml-3 mt-12 TITLE_BODONI">PRE-ORDER</p>
             <div className="grid sm:grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 pb-6 px-4   ">
                 <div className="focus:outline-none cardAll  creatorBlogCard" aria-label="card 2" id=" ">
                     <div className="overflow-hidden imgSize ">
