e62b87dae2ea3655d104c5d6baa56d9d58ab6346
devabuhasan <dev.abuhasan@gmail.com>
2022-03-10 09:05:26 -0500

    banner reponsive

--- components/Reuseable/Banner.tsx
+++ components/Reuseable/Banner.tsx
@@ -1,6 +1,6 @@
 import styles from '../../styles/Reuseable/Banner.module.scss';
 
-const Banner = ({ image, desc, textColor, title }: any) => {
+const Banner = ({ image, desc, textColor, title, icon }: any) => {
     // layer, defaultInfo, rightInfo, defaultClasses, icon
 
     return (
@@ -9,6 +9,7 @@ const Banner = ({ image, desc, textColor, title }: any) => {
             <div className="absolute pt-12  px-6 py-4" id={styles.mainClass}>
                 <div className={`mb-3 sm:text-4xl  xl:text-8xl md:text-8xl tracking-tight ${textColor}`}>{title}</div>
                 <div className={`leading-normal ${textColor}`}>{desc}</div>
+                <img className={`${styles.banner_icon}`} src={`${icon}`} alt="" />
             </div>
         </div>
     );
